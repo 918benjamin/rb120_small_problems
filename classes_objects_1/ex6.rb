b5e8bcb7b360dde93f434b47293ee87ac8c72819
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-01-15 10:33:22 -0600

    Add completed ex6 of classes and objects 1

--- classes_objects_1/ex6.rb
+++ classes_objects_1/ex6.rb
@@ -4,13 +4,31 @@
 # Expected output:
 # Hello! My name is Sophie!
 
+### Option 1 - manual getter
+# class Cat
+#   def initialize(name)
+#     @name = name
+#   end
+
+#   def name
+#     @name
+#   end
+
+#   def greet
+#     puts "Hello! My name is #{name}!"
+#   end
+# end
+
+### Option 2 - Attr_reader getter
 class Cat
+  attr_reader :name 
+
   def initialize(name)
     @name = name
   end
 
   def greet
-    puts "Hello! My name is #{@name}!"
+    puts "Hello! My name is #{name}!"
   end
 end
 
