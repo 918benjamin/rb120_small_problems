e44897875f63df52f1b43bdbb3f99f357140214c
Benjamin Perrault <benjamin.perrault@gmail.com>
2021-02-17 11:00:08 -0600

    Uncomment the execution to test the test cases

--- medium1/ex5.rb
+++ medium1/ex5.rb
@@ -97,38 +97,37 @@ end
 
 # Examples:
 
-# Copy Code
-# Minilang.new('PRINT').eval
+Minilang.new('PRINT').eval
 # # 0
 
-# Minilang.new('5 PUSH 3 MULT PRINT').eval
+Minilang.new('5 PUSH 3 MULT PRINT').eval
 # # 15
 
-# Minilang.new('5 PRINT PUSH 3 PRINT ADD PRINT').eval
+Minilang.new('5 PRINT PUSH 3 PRINT ADD PRINT').eval
 # # 5
 # # 3
 # # 8
 
-# Minilang.new('5 PUSH 10 PRINT POP PRINT').eval
+Minilang.new('5 PUSH 10 PRINT POP PRINT').eval
 # 10
 # 5
 
 # Minilang.new('5 PUSH POP POP PRINT').eval
 # Empty stack!
 
-# Minilang.new('3 PUSH PUSH 7 DIV MULT PRINT ').eval
+Minilang.new('3 PUSH PUSH 7 DIV MULT PRINT ').eval
 # # 6
 
-# Minilang.new('4 PUSH PUSH 7 MOD MULT PRINT ').eval
+Minilang.new('4 PUSH PUSH 7 MOD MULT PRINT ').eval
 # # 12
 
 # Minilang.new('-3 PUSH 5 XSUB PRINT').eval
 # # Invalid token: XSUB
 
-# Minilang.new('-3 PUSH 5 SUB PRINT').eval
+Minilang.new('-3 PUSH 5 SUB PRINT').eval
 # # 8
 
-# Minilang.new('6 PUSH').eval
+Minilang.new('6 PUSH').eval
 # (nothing printed; no PRINT commands)
 
 Minilang.new('6 PUSH DIV POP DIV').eval
